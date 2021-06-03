b3237021e15950d3309115eab532af564808fb71
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-04 01:13:46 +0600

    Login UI

--- App.js
+++ App.js
@@ -7,11 +7,21 @@ import { StyleSheet, Text, View } from 'react-native'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 const Stack = createStackNavigator()
+const globalScreenOptions = {
+  headerStyle: {backgroundColor: '#2c6bed'},
+  headerTitleStyle: {color: '#fff'},
+  headerTintColor: "#fff"
+}
 
 export default function App() {
   return (
     <NavigationContainer>
+<<<<<<< HEAD
       <Stack.Navigator>     
+=======
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        
+>>>>>>> 1072168 (Login UI)
         <Stack.Screen name='Login' component={LoginScreen} />
         <Stack.Screen name='Register' component={RegisterScreen} />
       </Stack.Navigator>
