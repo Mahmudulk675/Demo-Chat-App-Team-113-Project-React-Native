39569c7b749d10e1b3402190b6f117f06acd3592
Mahmudulk675 <mahmudulk675@gmail.com>
2021-06-04 15:04:32 +0600

    Add chat screen

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { StyleSheet, Text, View } from 'react-native'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import HomeScreen from './screens/HomeScreen'
+import AddChatScreen from './screens/AddChatScreen'
 const Stack = createStackNavigator()
 const globalScreenOptions = {
   headerStyle: { backgroundColor: '#2c6bed' },
@@ -19,6 +21,8 @@ export default function App() {
       <Stack.Navigator screenOptions={globalScreenOptions}>
         <Stack.Screen name='Login' component={LoginScreen} />
         <Stack.Screen name='Register' component={RegisterScreen} />
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='AddChatScreen' component={AddChatScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
